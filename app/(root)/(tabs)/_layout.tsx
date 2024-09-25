6fa7fa4b7a5c21cdf8ab1b9cda905e7fb430be36
Wilson635 <87822542+Wilson635@users.noreply.github.com>
2024-09-25 14:58:53 +0100

    initialize a new project

---
+++ app/(root)/(tabs)/_layout.tsx
@@ -0,0 +1,11 @@
+import { SafeAreaView, Text } from "react-native";
+
+const _Layout = () => {
+  return (
+    <SafeAreaView>
+      <Text>Layout</Text>
+    </SafeAreaView>
+  );
+};
+
+export default _Layout;
